90f162dd8e4b5d38bc536a6ae15451b984abfcb4
NPhillips4 <143303754+NPhillips4@users.noreply.github.com>
2023-09-17 16:51:23 -0400

    Lab3

---
+++ Powershell/lab3.ps1
@@ -0,0 +1,19 @@
+﻿function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*" 
+} 
+$IP = getIP 
+
+$USER = $env:USERNAME
+
+$HOSTNAME = hostname
+
+$SHELLVERSION = $HOST.Version.Major
+
+function getDate{
+    Get-Date -Format "dddd, MMMM dd, yyyy"
+    }
+$DATE = getDate
+
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. PowerShell Version $SHELLVERSION. Today's date is $DATE"
+
+$BODY | out-file C:\lab3.txt
