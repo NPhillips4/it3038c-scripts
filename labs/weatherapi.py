448e07e1e54e69441060686f4ec9e29b50be98f9
NPhillips4 <143303754+NPhillips4@users.noreply.github.com>
2023-11-05 16:13:55 -0500

    Delete labs/weatherapi.py

--- labs/weatherapi.py
+++
@@ -1,9 +0,0 @@
-import json 
-import requests 
-
-print('Please enter your zip code')
-zip = input()
-
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=c1b73a23d3dcca4643b775d589b1325b' % zip)
-data=r.json()
-print(data['weather'][0]['description'])
